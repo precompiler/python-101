5e675987a9a76430ad1fdade17044ae39fc8f38f
Richard Li <lishengyin.msn@hotmail.com>
2017-07-03 23:29:44 +0100

    Add more looping demos.

--- learning-python/ch02/Looping.py
+++ learning-python/ch02/Looping.py
@@ -4,3 +4,20 @@ for i in [1, 2, 3, 4]:
 for i in range(5):
     print(i)
 
+colors = ["red", "green", "blue"]
+for i in range(len(colors)):
+    print(i, colors[i])
+
+for color in colors:
+    print(color)
+
+for idx, color in enumerate(colors):
+    print(idx, color)
+
+people = ["Scott", "John", "Mike"]
+ages = [50, 30, 25]
+for person, age in zip(people, ages):
+    print(person, age)
+
+for data in zip(people, ages):
+    print(data)
