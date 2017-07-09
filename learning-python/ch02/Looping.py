f5a1e7f8e350a5f1b29c0e60caf178208946a2b1
Richard Li <lishengyin.msn@hotmail.com>
2017-07-09 19:06:37 +0100

    Add more samples.

--- learning-python/ch02/Looping.py
+++ learning-python/ch02/Looping.py
@@ -21,3 +21,44 @@ for person, age in zip(people, ages):
 
 for data in zip(people, ages):
     print(data)
+
+arr = [1, 2, 3, 4, 5, 6]
+print(arr)
+arr = arr[::-1]
+print(arr)
+
+empid = 2
+class NoResourceFoundException(Exception):
+    pass
+emps = {1: "Scott", 2: "John", 3: "Tiger"}
+for emp in emps.items():
+    if emp.__contains__(empid):
+        print("Found")
+        break
+else:
+    raise NoResourceFoundException("Not found")
+
+from itertools import count
+for n in count(5, 3):
+    if n > 20:
+        break
+    print(n, end=", ")
+print()
+from itertools import compress
+ret = compress("abcdefg", (1, 0, 1, 1))
+for x in ret:
+    print(x)
+
+data = range(10)
+even = [1, 0] * 10
+odd = [0, 1] * 10
+evenNumbers = compress(data, even)
+oddNumbers = compress(data, odd)
+print(list(data))
+print(list(evenNumbers))
+print(list(oddNumbers))
+
+from itertools import permutations, combinations
+print(list(permutations("ABC")))
+print(list(combinations("ABC", 2)))
+
