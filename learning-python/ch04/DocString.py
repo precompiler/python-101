1084e705b5d1bb292614353c4182439c1a79cf5e
precompiler <lishengyin.msn@hotmail.com>
2017-08-10 22:21:36 +0800

    Doc string demo

---
+++ learning-python/ch04/DocString.py
@@ -0,0 +1,14 @@
+def square(n):
+    """Return the square of a number n. """
+    return n ** 2
+
+
+def connect(host, port, user, password):
+    """Connect to a database.
+
+    :param host: host IP.
+    :param port: port
+    :param user: user name
+    :param password: password
+    :return: connection object.
+    """
