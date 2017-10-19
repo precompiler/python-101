d243ffcf0fbdbdd0227bdaf211f63729e76ea486
Richard Li <lishengyin.msn@hotmail.com>
2017-10-19 21:54:15 +0100

    Add collection demos.

--- mastering-python/ch03/collections.py
+++ mastering-python/ch03/collections.py
@@ -1,3 +1,4 @@
+#list
 l = list((1, 2, 3, 4, 5))
 l2 = list(range(10))
 l3 = [-1, -2, -3, -4]
@@ -8,4 +9,26 @@ filtered_list = list(filter(lambda num: num % 2 == 0, l2))
 
 print(filtered_list)
 
+print(l[0])
 
+print(l[1:4])
+
+tup_list = list((("key", "value"), ("key1", "value1")))
+for k, v in tup_list:
+    print(k, "->", v)
+
+#set
+colors = set(("red", "green", "blue"))
+for c in colors:
+    print(c)
+
+#dict
+d = dict()
+d["key"] = "value"
+d["key1"] = "value1"
+for key in d.keys():
+    print(d[key])
+
+#tuple
+t = ("a", 1, "c")
+print(t[2])
