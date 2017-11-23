41fb05011a83c95dfec71c8e9050ed0020ea1dfd
Richard Li <lishengyin.msn@hotmail.com>
2017-11-23 22:47:11 +0000

    Add reduce function demo.

--- mastering-python/ch04/FuncTools.py
+++ mastering-python/ch04/FuncTools.py
@@ -1,4 +1,6 @@
 import functools
+import collections
+import json
 
 
 def power(m, n):
@@ -11,3 +13,24 @@ square = functools.partial(power, n=2)
 print(square(2))
 
 print(square(10))
+
+
+def doSum(a, b):
+    return a + b
+
+print(functools.reduce(doSum, range(1, 100)))
+
+print(functools.reduce(lambda a, b: a + b, range(1, 100)))
+
+def tree():
+    return collections.defaultdict(tree)
+
+departments = tree()
+departments["DummyCompany"]["Finance"]["employees"] = ["Scott", "John", "Jane"]
+departments["DummyCompany"]["IT"]["employees"] = ["Tony", "Tiger"]
+print(json.dumps(departments, indent = 4))
+
+def getValue(path, depts):
+    return functools.reduce(lambda dts, key: dts[key], path.split("."), departments).items()
+
+print(getValue("DummyCompany.IT", departments))
